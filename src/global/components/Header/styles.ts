561d0aca213a9948ca87f753bbe8ba9729641863
Vinicius Italo <vinicius.italo@fretebras.com.br>
2023-02-03 22:50:21 -0300

    refactor: change colors in component Header and page Home

--- src/global/components/Header/styles.ts
+++ src/global/components/Header/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Header = styled.header`
-  background-color: ${({ theme }) => theme.colors.gray['700']};
+  background-color: ${({ theme }) => theme.colors.gray['800']};
   width: 100%;
   padding: 2rem;
   display: flex;
--- src/global/pages/Home/styles.ts
+++ src/global/pages/Home/styles.ts
@@ -4,6 +4,5 @@ export const Container = styled.main`
   max-width: 720px;
   width: 100%;
   margin: 6rem auto;
-  background-color: red;
   padding: 0 2rem;
 `;
