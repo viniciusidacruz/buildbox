4263efb3aa9531b4f549249f3a21586111b08ea0
Vinicius Italo <vinicius.italo@fretebras.com.br>
2023-02-03 21:18:06 -0300

    feat: create reset css

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { ThemeProvider } from 'styled-components';
-import { THEME_DEFAULT_TEMPLATE } from '@styles/index';
+import { ResetCSS, THEME_DEFAULT_TEMPLATE } from '@styles/index';
 
 export default function App() {
   return (
     <ThemeProvider theme={THEME_DEFAULT_TEMPLATE}>
+      <ResetCSS />
       <h1>Hello Wolrd</h1>
     </ThemeProvider>
   );
