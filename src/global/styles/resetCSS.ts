fd5c097e532fa2c7a695177a6708054b104bcb78
Vinicius Italo <vinicius.italo@fretebras.com.br>
2023-02-04 01:13:05 -0300

    refactor: implement css for field is disabled

--- src/global/styles/resetCSS.ts
+++ src/global/styles/resetCSS.ts
@@ -41,4 +41,9 @@ export const ResetCSS = createGlobalStyle`
             filter: brightness(1.3);
         }
     }
+
+    [disabled] {
+        cursor: not-allowed;
+        opacity: 0.6;
+    }
 `;
