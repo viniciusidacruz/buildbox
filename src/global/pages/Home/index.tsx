12637ad0438f99b6b3ce177aa9493b5132ed8462
Vinicius Italo <vinicius.italo@fretebras.com.br>
2023-02-03 22:31:20 -0300

    feat: create container for Home

--- src/global/pages/Home/index.tsx
+++ src/global/pages/Home/index.tsx
@@ -1,11 +1,16 @@
 import { Fragment } from 'react';
 
 import { HeaderComponent } from '@components/index';
+import { Container } from './styles';
 
 export function HomePage() {
   return (
     <Fragment>
       <HeaderComponent />
+
+      <Container>
+        <h1>Hello World</h1>
+      </Container>
     </Fragment>
   );
 }
---
+++ src/global/pages/Home/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.main`
+  max-width: 720px;
+  width: 100%;
+  margin: 6rem auto;
+  background-color: red;
+  padding: 0 2rem;
+`;
