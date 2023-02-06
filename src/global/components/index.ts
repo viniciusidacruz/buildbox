a48ff8ae6530687b16a8f980365b1549ac81fb44
Vinicius Italo <vinicius.italo@fretebras.com.br>
2023-02-05 22:44:59 -0300

    feat: create component for loading

--- src/global/components/index.ts
+++ src/global/components/index.ts
@@ -2,6 +2,7 @@ export * from './Logo';
 export * from './Form';
 export * from './Button';
 export * from './Header';
+export * from './Loading';
 export * from './TextArea';
 export * from './TextField';
 export * from './SelectImage';
