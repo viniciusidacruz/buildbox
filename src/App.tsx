2c362651a27fd35911a6560280727032d9a50d37
Vinicius Italo <vinicius.italo@fretebras.com.br>
2023-02-05 22:44:06 -0300

    feat: create context for manipulation refetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { ThemeProvider } from 'styled-components';
 
 import { AppRoutes } from '@config/routes';
+import { PostsProvider } from '@global/contexts/PostsContext';
 
 import { ResetCSS, THEME_DEFAULT_TEMPLATE } from '@styles/index';
 
 export default function App() {
   return (
-    <ThemeProvider theme={THEME_DEFAULT_TEMPLATE}>
-      <ResetCSS />
-      <AppRoutes />
-    </ThemeProvider>
+    <PostsProvider>
+      <ThemeProvider theme={THEME_DEFAULT_TEMPLATE}>
+        <ResetCSS />
+        <AppRoutes />
+      </ThemeProvider>
+    </PostsProvider>
   );
 }
